ca78fbacc407ce45457ffed7cd54feb8d7546930
thorc <chelsgieck@gmail.com>
2018-07-18 23:38:09 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ClickyGame from './ClickyGame';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ClickyGame />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
